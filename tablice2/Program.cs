327f16d6b030a65a1eb4861f066416c5c9f9febe
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-06-22 17:10:26 +0200

    Dodaj pliki projektów.

---
+++ tablice2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ile chcesz wpisać Liczb?");
+int rozmiar = Convert.ToInt32(Console.ReadLine());
+int[] liczby = new int[rozmiar];
+for (int i = 0; i < liczby.Length; i++)
+{
+    Console.WriteLine("Podaj {0} liczbe", i + 1);
+    liczby[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine();
+for (int i = 0; i < liczby.Length; i++)
+{
+
+    Console.Write(liczby[i] + ", ");
+}
+
+Console.ReadKey();
